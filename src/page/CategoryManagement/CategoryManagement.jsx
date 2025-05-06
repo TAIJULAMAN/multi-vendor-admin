7971c6a6aec8ec1db151e60ea64dc664b770d36f
Md Shah Aman Patwary <taijulaman8@gmail.com>
2025-05-06 14:48:23 +0600

    add modals in ads

--- src/page/CategoryManagement/CategoryManagement.jsx
+++ src/page/CategoryManagement/CategoryManagement.jsx
@@ -364,7 +364,7 @@ const CategoryManagement = () => {
         >
           <div className="p-5">
             <h1 className="text-4xl text-center text-[#0D0D0D]">
-              Are you sure you want to block ?
+              Are you sure you want to Delete ?
             </h1>
 
             <div className="text-center py-5">
@@ -372,7 +372,7 @@ const CategoryManagement = () => {
                 onClick={handleOk}
                 className="bg-[#14803c] text-white font-semibold w-full py-2 rounded transition duration-200"
               >
-                Yes,Block
+                Yes,Delete
               </button>
             </div>
             <div className="text-center pb-5">
@@ -380,7 +380,7 @@ const CategoryManagement = () => {
                 onClick={handleOk}
                 className="text-[#14803c] border-2 border-green-600 bg-white font-semibold w-full py-2 rounded transition duration-200"
               >
-                No,Don’t Block
+                No,Don’t Delete
               </button>
             </div>
           </div>
